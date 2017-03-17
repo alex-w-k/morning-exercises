d500ad76a678f100cfdf9491131e15b8d3ee8505
alex549us3 <alex549us3@gmail.com>
2017-03-17 12:43:29 -0600

    got fibonacci working

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,5 +1,17 @@
-first = 0
-second = 1
-26.times do |x|
-  first = first + second
+def fibonacci(n)
+  if n < 2
+    n
+  else
+    return fibonacci(n - 1) + (fibonacci(n-2))
+  end
 end
+
+puts "How many digits do you want to to output?"
+puts ""
+print ">"
+input = gets.chomp.to_i
+puts "---------------------------"
+input.times do |number|
+  puts fibonacci(number)
+end
+
