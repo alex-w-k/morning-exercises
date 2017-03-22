f687239f93a4f8700b2aff088af3b0a77c1ab692
alex549us3 <alex549us3@gmail.com>
2017-03-22 09:29:34 -0600

    morning exercise for 3/22/17

---
+++ night_shift.rb
@@ -0,0 +1,5 @@
+night_shifts = ["josh", "andrew", "sal", "casey", "beth", "ilana", "lauren",
+                "sal", "casey", "casey", "nate", "beth", "nate", "nate",
+                "casey", "casey", "casey", "andrew"]
+number_of_times = Hash.new(0)
+
